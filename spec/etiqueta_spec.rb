9277224c5d81c7e30efe666fafc8ca42f736b503
Marta García <alu0101015927@ull.edu.es>
2018-11-05 14:25:05 +0100

    Pruebas del almacenamientode las declaraciones obligatorias

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,9 +1,27 @@
 RSpec.describe Etiqueta do
-  it "has a version number" do
-    expect(Etiqueta::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  before :each do
+      @p1 = Etiqueta.new(0,1,2,3,4,5)
+    end
+
+    describe "# almacenamiento de las declaraciones obligatorias"
+      it "Se almacena correctamente la cantidad de grasas" do
+        expect(@p1.ngrasas).to eq(0)
+      end
+      it "Se almacena correctamente la cantidad de grasas saturadas" do
+        expect(@p1.ngrasas_s).to eq(1)
+      end
+      it "Se almacena correctamente los hidratos de carbono" do
+        expect(@p1.hidratos).to eq(2)
+      end
+      it "Se almacena correctamente los azucares" do
+        expect(@p1.azucares).to eq(3)
+      end
+      it "Se almacena correctamente las proteinas" do
+        expect(@p1.proteinas).to eq(4)
+      end
+      it "Se almacena correctamente la sal" do
+        expect(@p1.sal).to eq(5)
+      end
+
 end
