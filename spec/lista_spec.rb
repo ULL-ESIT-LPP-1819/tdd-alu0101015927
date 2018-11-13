e0e06a9835d192b307245eeb003e814404729858
Marta García <alu0101015927@ull.edu.es>
2018-11-13 19:05:25 +0100

    añadiendo el metodo empty

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -7,19 +7,21 @@ RSpec.describe Etiqueta do
     end
 
   describe "# creando la lista con numeros" do
+
     it "se inserta correctamente en la lista " do
 
       expect(@l.insert(1)).to be_a(Node)
     end
 
-    it "comprueba correctamente si está vacia" do
-      expect(@l.empty).to eq(false)
-    end
     it "se extrae correctamente de la lista" do
       expect(@l.extract).to be_a(Node)
     end
-    it "comprueba correctamente si está vacia" do
-      expect(@l.empty).to eq(true)
+
+    it "se comprueba correctamente si está vacia" do
+      expect(@l.empty).to be false
+      expect(@l.insert(1)).to be_a(Node)
+      expect(@l.extract).to be_a(Node)
+      expect(@l.empty).to be true
     end
   end
 
