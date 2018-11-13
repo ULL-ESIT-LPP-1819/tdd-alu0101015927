796212128a01277c14b5d8bb2dec0cb1fdc107f6
Marta García <alu0101015927@ull.edu.es>
2018-11-13 22:06:16 +0100

    añadido el metodo para recorrer la lista

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -3,32 +3,54 @@ require 'lib/lista.rb'
 RSpec.describe Etiqueta do
 
   before :each do
-      @l = List.new()
+
+      @e1 = Tag.new(0,0,0,0,0, 5 ,0,0,0,0,0,0,0)
+      @e2 = Tag.new(0,0,0,0,0, 6 ,0,0,0,0,0,0,0)
+      @e3 = Tag.new(0,0,0,0,0, 1 ,0,0,0,0,0,0,0)
+      @e4 = Tag.new(0,0,0,0,0, 2 ,0,0,0,0,0,0,0)
+      @e5 = Tag.new(0,0,0,0,0, 8 ,0,0,0,0,0,0,0)
+      @e6 = Tag.new(0,0,0,0,0, 7 ,0,0,0,0,0,0,0)
+      @e7 = Tag.new(0,0,0,0,0, 9 ,0,0,0,0,0,0,0)
+      @e8 = Tag.new(0,0,0,0,0, 3 ,0,0,0,0,0,0,0)
+      @e9 = Tag.new(0,0,0,0,0, 4 ,0,0,0,0,0,0,0)
+      @e10 = Tag.new(0,0,0,0,0, 10 ,0,0,0,0,0,0,0)
     end
 
-  describe "# creando la lista con numeros" do
+  describe "# creando la lista con etiquetas" do
 
     it "se inserta correctamente en la lista " do
-
-      expect(@l.insert(1)).to be_a(Node)
+      l = List.new()
+      expect(l.insert(@e1)).to be_a(Node)
     end
 
     it "se extrae correctamente de la lista" do
-      expect(@l.extract).to be_a(Node)
+      l = List.new()
+      expect(l.insert(@e1)).to be_a(Node)
+      expect(l.extract).to be_a(Node)
     end
 
     it "se comprueba correctamente si está vacia" do
-      expect(@l.empty).to be false
-      expect(@l.insert(1)).to be_a(Node)
-      expect(@l.extract).to be_a(Node)
-      expect(@l.empty).to be true
+      l = List.new()
+      expect(l.empty).to be true
+      expect(l.insert(@e1)).to be_a(Node)
+      expect(l.empty).to be false
+      expect(l.extract).to be_a(Node)
+      expect(l.empty).to be true
     end
 
     it "se comprueba correctamente el tamaño" do
-      expect(@l.length).to eq(0)
-      @l.insert(3)
-      @l.insert(7)
-      expect(@l.length).to eq(2)
+      l = List.new()
+      expect(l.length).to eq(0)
+      l.insert(@e1)
+      l.insert(@e2)
+      expect(l.length).to eq(2)
+    end
+
+    it "se devuelve correctamente el contenido" do
+      l = List.new()
+      l.insert(@e1)
+      l.insert(@e5)
+      expect(l.to_s).to be_a(String)
     end
   end
 
