521335dffe005a812cf2e99c48938b1d394a1412
Marta García <alu0101015927@ull.edu.es>
2018-11-19 20:28:32 +0100

    añadida la prueba del metodo clasificar

--- lib/etiqueta/individuo.rb
+++ lib/etiqueta/individuo.rb
@@ -1,4 +1,4 @@
-
+require "etiqueta/Nutricion.rb"
 class Individuo
   attr_accessor :name
 
@@ -9,7 +9,12 @@ class Individuo
 end
 
 class Paciente < Individuo
-
   attr_accessor :datos
+
+  def initialize(name, peso, talla, edad, sexo, ccintura, ccadera)
+    @name = name
+    @datos = Nutricion.new(peso, talla, edad, sexo, ccintura, ccadera)
+  end
+
 
 end
--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -1,11 +1,13 @@
 require 'lib/etiqueta/individuo.rb'
+require 'lib/etiqueta/lista.rb'
 
 RSpec.describe Etiqueta do
 
   context "# herencia" do
     before :each do
       @p1 = Individuo.new("Pepe")
-      @p2 = Paciente.new("Juan")
+      @p2 = Paciente.new("Juan", 1,2,3,4,5,6)
+
     end
 
     it "un objeto individuo es una instancia de la clase Individuo" do
@@ -16,6 +18,32 @@ RSpec.describe Etiqueta do
         expect(@p2).to be_an_instance_of(Paciente)
     end
 
+    it "clasificacion de pacientes en tratamiento de Obesidad" do
+      persona1 = Paciente.new("a", 1,2,3,4,5,6)
+      persona2 = Paciente.new("b", 1,2,3,4,5,6)
+      persona3 = Paciente.new("c", 1,2,3,4,5,6)
+      persona4 = Paciente.new("d", 1,2,3,4,5,6)
+      persona5 = Paciente.new("e", 1,2,3,4,5,6)
+      persona6 = Paciente.new("f", 1,2,3,4,5,6)
+      persona7 = Paciente.new("g", 1,2,3,4,5,6)
+      persona8 = Paciente.new("h", 1,2,3,4,5,6)
+      persona9 = Paciente.new("i", 1,2,3,4,5,6)
+      persona10 = Paciente.new("j", 1,2,3,4,5,6)
+
+      l = List.new
+      l.insert(persona1)
+      l.insert(persona2)
+      l.insert(persona3)
+      l.insert(persona4)
+      l.insert(persona5)
+      l.insert(persona6)
+      l.insert(persona7)
+      l.insert(persona8)
+      l.insert(persona9)
+      l.insert(persona10)
+
+        expect(clasificar_imc(l)).to eq(" ") 
+    end
 
   end
 
