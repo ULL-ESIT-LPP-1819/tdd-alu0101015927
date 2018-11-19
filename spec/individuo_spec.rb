0f0aff05bc3c1d9dfc950cc08dac9ed15208e90e
Marta García <alu0101015927@ull.edu.es>
2018-11-19 18:55:14 +0100

    creada la clase individuo

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Etiqueta do
 
   context "# herencia" do
     before :each do
-      @p1 = Individuo::Individuo.new("Pepe")
+      @p1 = Individuo.new("Pepe")
     end
 
     it "un objeto individuo es una instancia de la clase Individuo" do
-        expect(@p1).to be_an_instance_of(Individuo::Individuo)
+        expect(@p1).to be_an_instance_of(Individuo)
     end
 
   end
