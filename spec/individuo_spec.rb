ac0bd64e2f399b0c601256b285729106e07ea76b
Marta García <alu0101015927@ull.edu.es>
2018-11-19 19:01:38 +0100

    creando las pruebas de la clase paciente

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -5,12 +5,18 @@ RSpec.describe Etiqueta do
   context "# herencia" do
     before :each do
       @p1 = Individuo.new("Pepe")
+      @p2 = Paciente.new()
     end
 
     it "un objeto individuo es una instancia de la clase Individuo" do
         expect(@p1).to be_an_instance_of(Individuo)
     end
 
+    it "un objeto paciente es una instancia de la clase Paciente" do
+        expect(@p2).to be_an_instance_of(Paciente)
+    end
+
+    
   end
 
 end
