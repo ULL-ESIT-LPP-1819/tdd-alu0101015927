fb811a93fc4cec7692ae8750764b5125c7abc12e
Marta García <alu0101015927@ull.edu.es>
2018-11-19 18:45:54 +0100

    añadiendo las pruebas de un objeto de la clase individuo

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -2,4 +2,15 @@ require 'lib/etiqueta/individuo.rb'
 
 RSpec.describe Etiqueta do
 
+  context "# herencia" do
+    before :each do
+      @p1 = Individuo::Individuo.new("Pepe")
+    end
+
+    it "un objeto individuo es una instancia de la clase Individuo" do
+        expect(@p1).to be_an_instance_of(Individuo::Individuo)
+    end
+
+  end
+
 end
