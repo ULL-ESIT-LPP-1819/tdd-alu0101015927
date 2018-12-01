46cc8bd86199c0ac2d9cc23a459cd2b38c85b049
Marta García <alu0101015927@ull.edu.es>
2018-12-01 16:48:02 +0100

    añadidas las pruebas para enumerar lista de pacientes

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,3 +1,4 @@
+require './lib/etiqueta/lista.rb'
 RSpec.describe Etiqueta do
 
   context "# lista de etiquetas enumerables" do
@@ -41,4 +42,43 @@ RSpec.describe Etiqueta do
 
 
 
+
+    context "# lista de pacientes enumerables" do
+      before :each do
+
+        @persona1 = Paciente.new("a", 60,169,3,4,5,6)
+        @persona2 = Paciente.new("b", 120,160,3,4,5,6)
+        @persona3 = Paciente.new("c", 55,170,3,4,5,6)
+        @persona4 = Paciente.new("d", 78,150,3,4,5,6)
+        @persona5 = Paciente.new("e", 90,155,3,4,5,6)
+        @l = List.new
+        @l.insert(@p1)
+        @l.insert(@p2)
+        @l.insert(@p3)
+        @l.insert(@p4)
+        @l.insert(@p5)
+
+      end
+
+      it "comprobrando el metodo collect" do
+           expect(@l.collect{0}).to eq([0, 0, 0, 0, 0])
+     end
+
+      it "comprobando el metodo select" do
+        expect(@l.select{@persona1}).to eq([125, 150, 25, 50, 200])
+      end
+
+      it "comprobando el metodo max" do
+        expect(@l.max).to eq(200)
+      end
+
+      it "comprobando el metodo min" do
+        expect(@l.min).to eq(25)
+      end
+
+      it "comprobando el metodo sort" do
+        expect(@l.sort).to eq([25, 50, 125, 150, 200])
+      end
+
+    end
 end
