c4ab4515710330883dbf6bb3ecf4ca30b30964f4
Marta García <alu0101015927@ull.edu.es>
2018-12-12 10:37:57 +0100

    travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
-require "etiqueta"
+require "etiqueta/etiqueta"
+require "etiqueta/individuo"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
